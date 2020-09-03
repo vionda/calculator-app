a593dae9544d1601aef3d04a53f1fde536478f24
vionda <viondasalihu@gmail.com>
2020-09-03 15:40:15 +0200

    Connect calculator screen to code

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    //button actions
+    @IBAction func numberClicked(_ sender: UIButton){
+       
+    }
+    
+    @IBAction func operatorClicked(_ sender: UIButton){
+        switch sender.tag {
+        case 10:
+            print("add")
+        case 11:
+            print("minus")
+        case 12:
+            print("times")
+        case 13:
+            print("divide")
+        default:
+            return
+        }
+    }
+    
+    @IBAction func equalsClicked(_ sender: UIButton){
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
